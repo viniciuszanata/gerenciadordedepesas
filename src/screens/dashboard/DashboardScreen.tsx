83dfbca4043ff01223f98e56fd658c7651264128
vinicius.zanata <vinicius.zanata@idwall.co>
2023-07-04 23:56:20 -0300

    feat: dashboard screen

---
+++ src/screens/dashboard/DashboardScreen.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export const DashboardScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>DashboardScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
