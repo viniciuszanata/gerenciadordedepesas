d56d93b56dc46c0bdb4ae680d09ff61bf76c3f92
vinicius.zanata <vinicius.zanata@idwall.co>
2023-08-16 00:25:41 -0300

    feat: add buttons to manage screen

--- src/screens/dashboard/ManageExpenseScreen.tsx
+++ src/screens/dashboard/ManageExpenseScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useLayoutEffect} from 'react';
 import {View, StyleSheet} from 'react-native';
+import {ButtonInput} from '../../components/input/button';
 import {ButtonDelete} from '../../components/input/buttonDelete';
 
 interface ManageExpenseScreenProps {
@@ -26,8 +27,38 @@ const ManageExpenseScreen = ({route, navigation}: ManageExpenseScreenProps) => {
     }
   };
 
+  const handlerConfirmButton = () => {
+    try {
+      navigation.goBack();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handlerCancelButton = () => {
+    try {
+      navigation.goBack();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <ButtonInput
+          style={[styles.button, styles.cancelButton]}
+          onPress={handlerCancelButton}
+          title={'Cancel'}
+          titleStyle={[styles.buttonText, styles.cancelButtonText]}
+        />
+        <ButtonInput
+          style={styles.button}
+          onPress={handlerConfirmButton}
+          title={isEditing ? 'Update' : 'Add'}
+          titleStyle={styles.buttonText}
+        />
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <ButtonDelete onPress={handlerDeleteButton} />
@@ -42,6 +73,7 @@ export default ManageExpenseScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 24,
   },
   deleteContainer: {
     marginTop: 16,
@@ -50,7 +82,26 @@ const styles = StyleSheet.create({
     borderTopColor: '#bf00ff',
     alignItems: 'center',
   },
-  teste: {
-    backgroundColor: 'red',
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+    backgroundColor: '#3e04c3',
+    borderRadius: 4,
+    padding: 8,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  cancelButton: {
+    backgroundColor: 'transparent',
+  },
+  cancelButtonText: {
+    color: '#3e04c3',
   },
 });
